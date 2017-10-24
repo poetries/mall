f9000f1aee2e0eb3678de6c5117268e3e2c2afd5
poetries <jingguanliuye@gmail.com>
2017-10-25 00:39:32 +0800

    perfect webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,31 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+//环境变量的配置dev/online
+var WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
+
+//获取html-webpack-plugin的函数
+var getHtmlConfig = function(name, title) {
+    return {
+        template: './src/view/' + name + '.html',
+        filename: 'view/' + name + '.html',
+        favicon: './favicon.ico',
+        title: title,
+        inject: true,
+        hash: true,
+        chunks: ['common', name]
+    }
+};
+
+var config = {
     entry: {
         'common': ["./src/page/common/index.js"],
         'index': ["./src/page/index/index.js"],
-        'login': ["./src/page/login/index.js"]
+        'login': ["./src/page/user-login/index.js"]
     },
     output: {
         path: __dirname + '/dist/',
+        publicPath: 'dev' === WEBPACK_ENV ? '/dist/' : '//blog.poetries.top/mall/dist/',
         filename: "js/[name].js"
     },
     externals: {
@@ -37,6 +54,18 @@ module.exports = {
             }
         ]
     },
+    resolve: {
+        //文件后缀名
+        // extensions : ['','.js','.json'],
+        //文件别名
+        alias: {
+            node_modules: __dirname + '/node_modules',
+            util: __dirname + '/src/util',
+            page: __dirname + '/src/page',
+            service: __dirname + '/src/service',
+            image: __dirname + '/src/image',
+        }
+    },
     plugins: [
         // 独立通用模块
         new webpack.optimize.CommonsChunkPlugin({
@@ -50,3 +79,9 @@ module.exports = {
         new HtmlWebpackPlugin({ 'login': '用户登录' })
     ]
 }
+if ('dev' === WEBPACK_ENV) {
+    config.entry.common.push('webpack-dev-server/client?http://localhost:8088')
+
+}
+
+module.exports = config;
